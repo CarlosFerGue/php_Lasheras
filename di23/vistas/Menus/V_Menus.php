1bac83fb48c5b3385f16f3a1fbf0b641b8ced634
Carlos Fernández Guevara <131338727+CarlosFerGue@users.noreply.github.com>
2024-01-11 12:35:53 +0100

    Update V_Menus.php

--- di23/vistas/Menus/V_Menus.php
+++ di23/vistas/Menus/V_Menus.php
@@ -9,8 +9,18 @@
         echo '<td>' . $fila['nombre'] . '</td>';
         echo '<td>' . $fila['id_Padre'] . '</td>';
         echo '<td>' . $fila['accion'] . '</td>';
+        echo '<td>' . returnActivo($fila) . '</td>';
         echo '</tr>';
     }
     
     echo '</table>';
+
+    function returnPrivado($fila){
+        if () {
+            
+        }elseif(){
+
+        }
+
+    }
 ?>
