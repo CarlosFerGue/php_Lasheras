a51340e27de715ab4c665c34e4180abd6ceb1b0e
Carlos Fernández Guevara <131338727+CarlosFerGue@users.noreply.github.com>
2024-04-01 19:47:28 +0200

    it works

--- di23/js/Seguridad.js
+++ di23/js/Seguridad.js
@@ -23,12 +23,39 @@ function buscarMenusCards() {
         });
 }
 
-function añadirPermisoMenu(id_Menu) {
+function añadirPermisoMenu(id_Menu, b_nombre) {
+    // Obtener el ID del elemento
+    const idElemento = b_nombre.getAttribute("id");
 
     let opciones = { method: "GET" };
     let parametros = `controlador=Seguridad&metodo=añadirPermisoMenu&id_Menu=${id_Menu}`;
 
-    parametros += `&permiso=` + document.getElementById("b_permisoMenu").value;
+    parametros += `&permiso=` + document.getElementById(idElemento).value;
+
+    console.log(parametros);
+
+    fetch("C_Ajax.php?" + parametros, opciones)
+        .then(res => {
+            if (res.ok) {
+                console.log('Permiso añadido');
+                return res.text();
+            }
+        })
+        .then(vista => {
+            //document.getElementById("CapaResultadoBusqueda").innerHTML = vista;
+        })
+        .catch(err => {
+            console.log("Error al realizar la petición", err.message);
+        });
+}
+
+
+function añadirPermisoSubMenu(id_Menu) {
+
+    let opciones = { method: "GET" };
+    let parametros = `controlador=Seguridad&metodo=añadirPermisoMenu&id_Menu=${id_Menu}`;
+
+    parametros += `&permiso=` + document.getElementById("b_permisoSubMenu").value;
 
     console.log(parametros);
 
--- di23/vistas/Menus/V_MttoMenus_Listado.php
+++ di23/vistas/Menus/V_MttoMenus_Listado.php
@@ -11,18 +11,24 @@ foreach ($menus as $menu) {
         <div class="tarjeta">
             <h3><?php echo $menu['nombre']; ?></h3>
             <div class="permisos">
-                <button type="button" id="botonPermisoMenu" onclick="añadirPermisoMenu(<?php echo $menu['id_Menu']; ?>)">Añadir permiso</button>
+            <button type="button" id="botonPermisoMenu" onclick="añadirPermisoMenu(<?php echo $menu['id_Menu']; ?>,<?php echo $menu['nombre']; ?>)">Añadir permiso</button>
                 <label for="b_permisoMenu"></label>
-                <input type="text" id="b_permisoMenu" name="b_permisoMenu">
+                <input type="text" id="<?php echo $menu['nombre']; ?>" name="b_permisoMenu">
             </div>
 
             <!-- Buscar y mostrar submenús -->
             <?php foreach ($menus as $submenu) {
                 if ($submenu['id_Padre'] == $menu['id_Menu']) { ?>
                     <div class="subtarjeta">
-                        <p><?php echo $submenu['nombre']; ?></p>
-                        <!-- Mostrar el contenido adicional de la subtarjeta si es necesario -->
-                        <button class="boton_subpermiso" onclick="añadirPermisoSubmenu(<?php echo $submenu['id_Menu']; ?>)">Añadir permiso submenu</button>
+                        <!-- <p><?php //echo $submenu['nombre']; ?></p>
+                        <button class="boton_subpermiso" onclick="añadirPermisoSubmenu(<?php //echo $submenu['id_Menu']; ?>)">Añadir permiso submenu</button>-->
+                        <div class="permisos">
+                            <button type="button" id="botonPermisoSubMenu" onclick="añadirPermisoSubmenu(<?php echo $submenu['id_Menu']; ?>)">Añadir permiso submenu</button>
+                            <label for="b_permisoSubMenu"></label>
+                            <input type="text" id="b_permisoSubMenu" name="b_permisoSubMenu">
+                        </div>
+
+
                     </div>
             <?php }
             } ?>
